ddea6cd0894ae3658ab8c8f99d74b0f0bca870ed
askupkov <alexfeed05@mail.ru>
2022-11-05 15:46:16 +0300

    5.1 major rework

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -1,11 +1,16 @@
 # вариант 12
 # составить функцию суммирования числового ряда
 def summ(a):
-    return sum(a))
+    summa = 0
+    i = len(a)
+    while i > 0:
+        summa += a[i-1]
+        i -= 1
+    return summa
 
 
 try:
-    nums = map(int, input('Введите ряд чисел через пробел: ').split())
+    nums = list(map(int, input('Введите ряд чисел через пробел: ').split()))  # ввод чисел через пробел в список
     print(summ(nums))
 except Exception as ex:
     print('Произошло исключение: ', ex)
