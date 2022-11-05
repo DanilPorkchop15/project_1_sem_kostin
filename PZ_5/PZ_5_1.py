b858d3cf6f7d19e087490b6e340555057978901d
Danil Kostin <114452500+DanilPorkchop15@users.noreply.github.com>
2022-11-05 16:50:02 +0300

    Update PZ_5_1.py

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -1,17 +1,17 @@
 # вариант 12
 # составить функцию суммирования числового ряда
 def summ(a):
+    i = 0
     summa = 0
-    i = len(a)
-    while i > 0:
-        summa += a[i-1]
-        i -= 1
+    while a > i:
+        i += 1
+        summa += i
     return summa
 
 
 while True:
     try:
-        nums = list(map(int, input('Введите ряд чисел через пробел: ').split()))  # ввод чисел через пробел в список
+        nums = int(input('Введите ряд чисел через пробел: '))  # ввод длины числового ряда
         print(summ(nums))
         break
     except Exception as ex:
