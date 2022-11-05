3e8832f8256319f6e0a7349539a69c4ea5bf6baf
askupkov <alexfeed05@mail.ru>
2022-11-05 15:51:39 +0300

    5.2 код добавлен

---
+++ PZ_5/PZ_5_2.py
@@ -0,0 +1,16 @@
+# Вариант 12
+# составить функцию SORTDEC3, которая сортирует по убыванию ряд из трех чисел и возвразщает сортированные значения
+def sortdec3(a, b, c):
+    a, b, c = max(a, b, c), a + b + c - max(a, b, c) - min(a, b, c), min(a, b, c)
+    return a, b, c
+
+
+while True:
+    try:
+        a1, b1, c1 = map(float, input('Введите через пробел три вещественных числа(a1, b1, c1): ').split())
+        print(sortdec3(a1, b1, c1))
+        a2, b2, c2 = map(float, input('Введите через пробел три вещественных числа(a2, b2, c2): ').split())
+        print(sortdec3(a2, b2, c2))
+        break
+    except ValueError:
+        print('Вы неправильно ввели значения, следуйте интрукциям во избежание дальнейших ошибок.\nПопробуйте заново.')
