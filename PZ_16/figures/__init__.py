a51010643270d6e09515cd70c25083c65a537fb9
s0rrel2711 <s0rrel2711@mail.ru>
2023-02-27 18:17:05 +0300

    pz 16 code

--- PZ_16/figures/__init__.py
+++ PZ_16/figures/__init__.py
@@ -1,5 +1,5 @@
-from .circle.code import circle_area, circle_perimetr
-from .square.code import square_area, square_perimeter
-from .triangle.code import triangle_area, triangle_perimeter
+from .circle.code import *
+from .square.code import *
+from .triangle.code import *
 
 NAME = 'Периметры и площади фигур'
